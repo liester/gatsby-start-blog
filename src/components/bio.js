0174021fe8b66f070b4c8658a34bedd9aabb601a
Levi Liester <levi.liester@gmail.com>
2023-01-22 20:42:32 -0600

    This shows the salty duck egg picture

--- src/components/bio.js
+++ src/components/bio.js
@@ -41,28 +41,12 @@ const Bio = () => {
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
   const social = data.site.siteMetadata?.social
-
+    const image = data.allMarkdownRemark?.nodes[2]?.frontmatter.photo.childImageSharp.gatsbyImageData.images.sources[0].srcSet;
+    console.log({data})
+  console.log({image})
   return (
     <div className="bio">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      />
-      {author?.name && (
-        <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
-        </p>
-      )}
+      <img srcset={image} width={100} height={100}/>
     </div>
   )
 }
